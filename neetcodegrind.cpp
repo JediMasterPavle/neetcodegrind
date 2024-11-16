f78f113cb10333b3c06cf66ceb8dc43a1eca5e6e
Pavle Kotarac <pavlekotarac@hotmail.com>
2024-11-17 00:48:21 +0100

    Added simple comment explaining what this class's purpose is

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+/*
+This is the main function which doesn't do anything,
+the functions/classes above will be answers to NeetCode questions
+*/
 int main(){
     cout<<"Neet Code Grind";
     return 0;
