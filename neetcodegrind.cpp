17929ef98d6e4c27392b6d27b8937a57a35326c9
Pavle Kotarac <pavlekotarac@hotmail.com>
2024-11-18 00:00:34 +0100

    Solution: Has Duplicate
    Optimal solution for "Has Duplicate" leet code/neet code question
    that uses std::set to check duplicate elements (O(1)) providing a solution
    that is O(n) space and time complexity

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <set>
+
 using namespace std;
+
+/*
+"Contains Duplicate" Leet Code Question: https://leetcode.com/problems/contains-duplicate/description/
+*/
+bool hasDuplicate(std::vector<int>& nums) {
+        std::set<int> set;
+        for (int& i : nums) {
+            if (set.find(i) != set.end()) {
+                return true;
+            }
+            set.insert(i);
+        }
+        return false;
+}
 
 /*
 This is the main function which doesn't do anything,
