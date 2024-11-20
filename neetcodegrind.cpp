06c547aa1b12d812e892fcae4987bdacfb7a38f6
Pavle Kotarac <pavlekotarac@hotmail.com>
2024-11-20 20:35:36 +0100

    Problem: Two Sum
    Looping through list and storing key/value pair (element/index).
    Before the storing whe check if the (trget - current_element) is in
    the map, if it is true that mean we have a pair of elements that
    add up to the target and we return the pair of indices. If we loop
    through the list and no pair is found return an empty list

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
-#include <vector>
+#include <unordered_map>
 #include <set>
+#include <vector>
 
 using namespace std;
 
 /*
-"Contains Duplicate" Leet Code Question: https://leetcode.com/problems/contains-duplicate/description/
+Problem: Contains Duplicate
+Leet Code Question: https://leetcode.com/problems/contains-duplicate/description/
 */
-bool hasDuplicate(std::vector<int>& nums) {
+bool hasDuplicate(std::vector<int>& nums)
+{
         std::set<int> set;
-        for (int& i : nums) {
-            if (set.find(i) != set.end()) {
+        for (int& i : nums)
+        {
+            if (set.find(i) != set.end())
+            {
                 return true;
             }
             set.insert(i);
         }
+
         return false;
 }
+
 /*
-"Valid Anagram" Leet Code Question: https://leetcode.com/problems/valid-anagram/description/
+Problem: Valid Anagram
+Leet Code Link: https://leetcode.com/problems/valid-anagram/description/
 */
-bool isAnagram(string s, string t) {
-    
+bool isAnagram(string s, string t)
+{
     if (s.length() != t.length())
         return false;
 
     vector<int> count(26,0);
     int length = s.length();
-    for(int i = 0;i<length;i++){
+
+    for(int i = 0;i<length;i++)
+    {
         count[s[i]-'a'] +=1;
         count[t[i]-'a'] -=1;
     }
 
-    for (int val : count) {
+    for (int val : count)
+    {
         if (val != 0) {
             return false;
         }
@@ -42,10 +53,34 @@ bool isAnagram(string s, string t) {
 }
 
 /*
+Problem: Two Sum
+Leet Code Link: https://leetcode.com/problems/two-sum/description/
+*/
+vector<int> twoSum(vector<int>& nums, int target)
+{
+    if (nums.size() == 0)
+        return {};
+
+    std::unordered_map<int, int> twoSumMap;
+    int numsSize = nums.size();
+    for (int i=0; i < numsSize; i++)
+    {
+        int complement = target - nums[i];
+        if (twoSumMap.find(complement) != twoSumMap.end())
+            return {twoSumMap[complement],i};
+
+        twoSumMap[nums[i]] = i;
+    }
+
+    return {};
+}
+
+/*
 This is the main function which doesn't do anything,
 the functions/classes above will be answers to NeetCode questions
 */
-int main(){
+int main()
+{
     cout<<"Neet Code Grind";
     return 0;
 }
