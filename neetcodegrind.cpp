9b62ff723c886f918d0212b94b73e5ebcfde0c0d
Pavle Kotarac <pavlekotarac@hotmail.com>
2024-11-24 20:45:01 +0100

    Top K Frequent Elements
    This function finds the k most frequent elements in a given list nums. It first counts the frequency
    of each element using an unordered map. Then, it organizes these elements by their frequency in a
    vector of vectors, where the index represents the frequency, and each slot contains elements with that
    frequency. After that, it iterates through the frequency vector from highest to lowest frequency, collecting
    elements into the result vector until k elements are added. Finally, it returns the list of the top k
    frequent elements. The approach

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -109,6 +109,40 @@ vector<int> twoSum(vector<int>& nums, int target)
     return {};
 }
 
+/*
+Problem: Top K Frequent Elements
+Leet Code Link: https://leetcode.com/problems/top-k-frequent-elements/
+*/
+vector<int> topKFrequent(vector<int>& nums, int k)
+{
+    unordered_map<int, int> count;
+    vector<vector<int>> freq(nums.size() + 1);
+
+    for (int n : nums)
+    {
+        count[n] = 1 + count[n];
+    }
+
+    for (const auto& entry : count)
+    {
+        freq[entry.second].push_back(entry.first);
+    }
+
+    vector<int> res;
+    for (int i = freq.size() - 1; i > 0; --i)
+    {
+        for (int n : freq[i])
+        {
+            res.push_back(n);
+            if (res.size() == k)
+            {
+                return res;
+            }
+        }
+    }
+    return res;
+}
+
 
 
 /*
