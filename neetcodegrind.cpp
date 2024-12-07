cc9e9c5a4e32e506bde25f4a4d06f6a4d3170189
Pavle Kotarac <pavlekotarac@hotmail.com>
2024-12-07 23:54:09 +0100

    Problem: Insert into a Binary Search Tree
    The code is a recursive implementation to insert a value into a Binary Search Tree (BST).
    It first checks if the tree is empty (root == nullptr), and if so, it creates a new node
    with the given value. If the tree is not empty, it compares the value to be inserted (val)
    with the current node's value (root->val). If val is less than root->val, the algorithm
    recursively calls insertIntoBST on the left subtree. If val is greater, it recursively
    calls insertIntoBST on the right subtree. This process ensures the value is inserted in
    the correct position to maintain the BST property: values smaller than the current node
    go to the left, and values larger go to the right. Finally, it returns the root of the
    modified tree. This approach builds the tree while preserving the binary search property
    but does not necessarily balance the tree.

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -713,6 +713,29 @@ TreeNode* searchBST(TreeNode* root, int val)
     }
 }
 
+/*
+Problem: Insert into a Binary Search Tree
+Leet Code Link: https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+*/
+TreeNode* insertIntoBST(TreeNode* root, int val)
+{
+    if (root == nullptr)
+    {
+        return new TreeNode(val);
+    }
+
+    if (val < root->val)
+    {
+        root->left = insertIntoBST(root->left, val);
+    }
+    else if (val > root->val)
+    {
+        root->right = insertIntoBST(root->right, val);
+    }
+
+    return root;
+}
+
 
 /*
 This is the main function which doesn't do anything,
