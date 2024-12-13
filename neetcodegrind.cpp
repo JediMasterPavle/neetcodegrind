9a93abb2ed068e1e02a4ba5f9517df0b4c03e25a
Pavle Kotarac <pavlekotarac@hotmail.com>
2024-12-13 22:12:55 +0100

    Problem: Subsets
    This code generates all subsets of a given list nums using
    depth-first search (DFS) and backtracking. The subsets function
    initializes an empty result vector res and calls dfs to explore
    subsets starting from index 0. In the dfs function, the base case
    adds the current subset to res when all elements have been considered.
    For each element, the function recursively explores two possibilities:
    including the element in the subset (subset.push_back(nums[i])) or
    excluding it (subset.pop_back()). After exploring one path, pop_back()
    restores the state of subset to explore the next possibility, ensuring
    all subsets are considered. This is backtracking because the algorithm
    explores all paths by adding elements, recursing, and then undoing
    the changes to try other possibilities.

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -1165,6 +1165,34 @@ bool hasPathSum(TreeNode* root, int targetSum)
     return false;
 }
 
+/*
+Problem: Subsets
+Leet Code Link: https://leetcode.com/problems/subsets/description/
+*/
+void dfs(const vector<int>& nums, int i, vector<int>& subset, vector<vector<int>>& res)
+{
+    if (i >= nums.size())
+    {
+        res.push_back(subset);
+        return;
+    }
+
+    subset.push_back(nums[i]);
+    dfs(nums, i+1, subset, res);
+
+    subset.pop_back();
+    dfs(nums, i+1, subset, res);
+}
+
+vector<vector<int>> subsets(vector<int>& nums)
+{
+    vector<vector<int>> res;
+    vector<int> subset;
+    dfs(nums, 0, subset, res);
+    return res;
+}
+
+
 
 
 /*
