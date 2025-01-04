63c070f0035220b8c96f55bb449932e1bbcc33fe
Pavle Kotarac <pavlekotarac@hotmail.com>
2025-01-04 22:23:27 +0100

    Problem: Longest Common Subsequence

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -2154,6 +2154,32 @@ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
     return dp[0];
 }
 
+/*
+Problem: Longest Common Subsequence
+Leet Code Link: https://leetcode.com/problems/longest-common-subsequence/
+*/
+int longestCommonSubsequence(string text1, string text2)
+{
+    vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
+
+    for (int i = text1.size() - 1; i >= 0; i--)
+    {
+        for (int j = text2.size() - 1; j >= 0; j--)
+        {
+            if (text1[i] == text2[j])
+            {
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+    }
+
+    return dp[0][0];
+}
+
 
 
 
