47c38b6cff1640b22369d48a05c40988315261ec
Pavle Kotarac <pavlekotarac@hotmail.com>
2025-01-05 18:38:55 +0100

    Problem: Resverse Bits
    Simple function which reveses bits of a unsigned 32bit integer
    in O(1) time and space complexity

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -2215,6 +2215,23 @@ vector<int> countBits(int n)
     return dp;
 }
 
+/*
+Problem: Resverse Bits
+Leet Code Link: https://leetcode.com/problems/reverse-bits/
+*/
+uint32_t reverseBits(uint32_t n)
+{
+    uint32_t res = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        uint32_t bit = (n >> i) & 1;
+        res |= (bit << (31 - i));
+    }
+
+    return res;
+}
+
+
 
 
 
