a12d8593bb93f362d7c96038e843d19794fa9c62
Pavle Kotarac <pavlekotarac@hotmail.com>
2025-03-13 20:52:57 +0100

    Problem: Linked List Cycle II
    We first find a node where fast and slow intersect and once the find is
    ther fast is resedt to head node and increments one by one like slow
    until fast and slow intersect again which will be the start of the cycle
    (Floyd's Tortoise and Hare Algorithm). This approach is O(n) time and
    O(1) space complexity.

--- neetcodegrind.cpp
+++ neetcodegrind.cpp
@@ -2609,6 +2609,38 @@ int pairSum(ListNode* head)
     return maximumSum;
 }
 
+/*
+Problem: Linked List Cycle II
+Leet Code Link: Problem: https://leetcode.com/problems/linked-list-cycle-ii/description/
+*/
+ListNode *detectCycle(ListNode *head)
+{
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            break;
+        }
+    }
+
+    if (fast == nullptr || fast->next == nullptr)
+        return nullptr;
+
+    fast = head;
+    while (fast != slow)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return fast;
+}
+
 
 
 
